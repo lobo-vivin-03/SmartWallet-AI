13679f93f33c7711328b76b8785b16163cd48685
VivinLobo <160881322+VivinLobo@users.noreply.github.com>
2024-08-28 00:26:55 +0530

    gf
    vgyvyg

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+      apiKey: "AIzaSyBQJfSWIPHz6oNbCnUxQx_5WqrPNK253-Y",
+      authDomain: "clone-a1141.firebaseapp.com",
+      projectId: "clone-a1141",
+      storageBucket: "clone-a1141.appspot.com",
+      messagingSenderId: "63592198206",
+      appId: "1:63592198206:web:b2184997c95eac602e216f",
+      measurementId: "G-F5X4SN00NG"
+    };
