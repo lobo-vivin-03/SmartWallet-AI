423af1c894f1feb6331945ffc6b1d0fa57bbbf9c
lobo-vivin-03 <vivinlobo5@gmail.com>
2024-09-27 18:56:35 +0530

    commit

---
+++ my-app/src/app/_components/Header.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
